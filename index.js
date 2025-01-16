2f6784ea45acd2e06a3e763cec4238afa4efeb03
Irfan Ishtiaq <irfan.ishtiaq@fcec.sa>
2025-01-16 11:56:21 +0300

    nine

--- index.js
+++ index.js
@@ -16,24 +16,9 @@ function closeabout(){
 
 
 function showwork(){
-    $("#work_container").css("display","flex");
+    $("#work_container").css("display","inherit");
     $("#work_container").addClass("animated slideInRight");
-
     setTimeout(function(){
-        // Initialize the Swiper once the work_container is displayed
-        var swiper = new Swiper('.swiper-container', {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
-
         $("#work_container").removeClass("animated slideInRight");
     },800);
 }
